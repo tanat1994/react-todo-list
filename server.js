ecf29a23f62d14044820401685e081a6fd1d7b9a
tanat1994 <tanat1994@gmail.com>
2018-06-19 10:29:56 +0700

    fixed server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // @dev render Inder.html
 app.get('*', (req, res) => {
